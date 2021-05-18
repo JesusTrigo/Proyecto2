118d1a82c6adbeef9ef29fabdc311172ca258654
Jesús <jesuscell89@gmail.com>
2021-05-18 21:43:08 +0200

    tablas acabadas

--- functionStats.js
+++ functionStats.js
@@ -27,7 +27,7 @@
 stats(data.matches);
 
 function stats(matches) {
-    
+
     let estadisticas = [];
 
     for (let i = 0; i < matches.length; i++) {
@@ -49,71 +49,253 @@ function stats(matches) {
         let goalsHome = matches[i].score.fullTime.homeTeam;
 
         let goalsAway = matches[i].score.fullTime.awayTeam;
-        
+
         let homeTeamEcontrado
 
         let awayTeamEcontrado
 
         let media
 
-        for(let j=0; j < estadisticas.length; j++ ) {
+        for (let j = 0; j < estadisticas.length; j++) {
 
 
-            if(idAway ==  estadisticas[j].id) {
+            if (idAway == estadisticas[j].id) {
                 awayTeamEcontrado = estadisticas[j]
 
             }
-            
-            if(idHome ==  estadisticas[j].id) {
+
+            if (idHome == estadisticas[j].id) {
                 homeTeamEcontrado = estadisticas[j]
-            }    
+            }
         }
-        
-        if(awayTeamEcontrado == undefined){
-        
-            
+
+        if (awayTeamEcontrado == undefined) {
+
+
             let objeto = {
                 id: idAway,
                 name: nameAway,
                 goals: goalsAway,
-                matches: 0,
+                matches: 1,
                 avg: media
-                
-                
+
+
             }
-            estadisticas.push(objeto); 
-        }else {
+            estadisticas.push(objeto);
+        } else {
             awayTeamEcontrado.goals += goalsAway;
             awayTeamEcontrado.matches++;
         }
 
 
-        if(homeTeamEcontrado == undefined){
-            
+        if (homeTeamEcontrado == undefined) {
+
             let objeto = {
                 id: idHome,
                 name: nameHome,
                 goals: goalsHome,
-                matches: 0,
+                matches: 1,
                 avg: media
-                
+
             }
-            estadisticas.push(objeto); 
-        }else {
+            estadisticas.push(objeto);
+        } else {
             homeTeamEcontrado.goals += goalsHome;
             homeTeamEcontrado.matches++;
         }
-    
+
     }
 
-    
-    for (let k=0; k < estadisticas.length; k++ ){
-            
+    for (let k = 0; k < estadisticas.length; k++) {
+
         let media = estadisticas[k].goals / estadisticas[k].matches;
-        
+
         estadisticas[k].avg = media;
-        
-        
+
     }
-    console.log(estadisticas);
+
+    estadisticas.sort((a, b) => b.avg - a.avg);
+
+    creartabla(estadisticas);
+
+
 };
+
+
+function creartabla(estadisticas) {
+
+    let top5 = estadisticas.slice(0, 5);
+
+
+    var tbody = document.getElementById('tablastats');
+
+    for (let i = 0; i < top5.length; i++) {
+
+        const row = document.createElement('tr');
+
+        let equipo = top5[i].id;
+
+        let escudo = document.createElement('img');
+
+        escudo.setAttribute("src", "https://crests.football-data.org/" + equipo + ".svg")
+
+        //escudo.classList.add (añadir clase);
+
+        let array = [
+            escudo,
+            top5[i].name,
+            top5[i].goals,
+            top5[i].matches,
+            top5[i].avg.toFixed(2)
+        ]
+
+        for (let j = 0; j < array.length; j++) {
+
+
+            const td = document.createElement('td');
+
+            td.append(array[j]);
+
+            row.append(td);
+            tbody.append(row);
+        }
+
+    }
+}
+
+
+
+
+
+
+encontra(data.matches);
+
+function encontra(matches) {
+
+    let estadisticas = [];
+
+    for (let i = 0; i < matches.length; i++) {
+
+        let estadoPartido = matches[i].status;
+
+        if (estadoPartido !== "FINISHED") {
+            continue
+        }
+
+        let idAway = matches[i].awayTeam.id;
+
+        let nameAway = matches[i].awayTeam.name;
+
+        let goalsHome = matches[i].score.fullTime.homeTeam;
+
+        //let homeTeamEcontrado
+
+        let awayTeamEcontrado
+
+        let media
+
+        for (let j = 0; j < estadisticas.length; j++) {
+
+
+            if (idAway == estadisticas[j].id) {
+                awayTeamEcontrado = estadisticas[j]
+
+            }
+
+            // if (idHome == estadisticas[j].id) {
+            //     homeTeamEcontrado = estadisticas[j]
+            // }
+        }
+
+        if (awayTeamEcontrado == undefined) {
+
+
+            let objeto = {
+                id: idAway,
+                name: nameAway,
+                goals: goalsHome,
+                matches: 1,
+                avg: media
+
+
+            }
+            estadisticas.push(objeto);
+        } else {
+            awayTeamEcontrado.goals += goalsHome;
+            awayTeamEcontrado.matches++;
+        }
+
+
+        // if (homeTeamEcontrado == undefined) {
+
+        //     let objeto = {
+        //         id: idHome,
+        //         name: nameHome,
+        //         goals: goalsHome,
+        //         matches: 0,
+        //         avg: media
+
+        //     }
+        //     estadisticas.push(objeto);
+        // } else {
+        //     homeTeamEcontrado.goals += goalsHome;
+        //     homeTeamEcontrado.matches++;
+        // }
+
+    }
+
+    for (let k = 0; k < estadisticas.length; k++) {
+
+        let media = estadisticas[k].goals / estadisticas[k].matches;
+
+        estadisticas[k].avg = media;
+
+    }
+
+    estadisticas.sort((a, b) => a.goals - b.goals);
+
+    creartabla2(estadisticas);
+
+
+};
+
+function creartabla2(estadisticas) {
+
+    let top5 = estadisticas.slice(0, 5);
+
+
+    var tbody = document.getElementById('tablacontra');
+
+    for (let i = 0; i < top5.length; i++) {
+
+        const row = document.createElement('tr');
+
+        let equipo = top5[i].id;
+
+        let escudo = document.createElement('img');
+
+        escudo.setAttribute("src", "https://crests.football-data.org/" + equipo + ".svg")
+
+        //escudo.classList.add (añadir clase);
+
+        let array = [
+            escudo,
+            top5[i].name,
+            top5[i].goals,
+            top5[i].matches,
+            
+        ]
+
+        for (let j = 0; j < array.length; j++) {
+
+
+            const td = document.createElement('td');
+
+            td.append(array[j]);
+
+            row.append(td);
+            tbody.append(row);
+        }
+
+    }
+}
